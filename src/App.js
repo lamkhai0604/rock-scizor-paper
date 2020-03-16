7570b29793efdc9ed44846740b451fb4631b66f1
your_name <lamkhai0604@gmail.com>
2020-03-16 16:51:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import Box from './component/Box'
 
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Box name="Player" color="red" winner={false} imgURL={choices.rock} />
+      <Box name="Computer" color="orange" winner={true} imgURL={choices.paper} />
+
+
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
 
 export default App;
---
+++ src/component/Box.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Box(props){
+    return(
+        <div
+  className={`box ${props.winner ? 'winner' : 'loser'}`}
+>
+  <h1>{props.name}</h1>
+  <img src={props.imgURL} alt={props.title}/>
+  <h3>{props.winner ? 'Won' : 'Loss'}</h3>
+</div>
+    )
+}
+
+// import React, { Component } from 'react'
+
+// class Box extends Component {
+//         render() {
+//                 return (
+//                         <div className={this.props.color}>
+//                             <h1>{this.props.name}</h1>
+//                 <div>{this.props.color}</div>
+//                 <div>{this.props.win?"Won":"Loss"} </div>
+//             </div>
+//         )
+//     }
+// }
+
+export default Box;
